925b15c2306b12231e253e056ed4de0079dbd4ea
botro-plays <aquariusbotro@gmail.com>
2026-06-04 08:55:45 +0800

    fix(wallet): import EventsModule so WalletService dependency resolves at runtime

--- apps/api/src/modules/wallet/wallet.module.ts
+++ apps/api/src/modules/wallet/wallet.module.ts
@@ -4,10 +4,11 @@ import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
 import { CurrencyService } from './currency.service';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { EventsModule } from '../events/events.module';
 import { PayMongoModule } from '../paymongo/paymongo.module';
 
 @Module({
-  imports: [NotificationsModule, forwardRef(() => PayMongoModule)],
+  imports: [NotificationsModule, EventsModule, forwardRef(() => PayMongoModule)],
   controllers: [WalletController],
   providers: [WalletService, CurrencyService],
   exports: [WalletService, CurrencyService],
